334c296aaadbb5090d92ffd5afffb6f619126bd6
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-06-12 13:51:06 +0700

    add: send to whatsapp function

--- app/src/main/java/com/frogobox/research/MainActivity.kt
+++ app/src/main/java/com/frogobox/research/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.frogobox.research
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URLEncoder
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_1).setOnClickListener {
+            val contact = "+62" // use country code with your phone number
+            val message = "Hallo"
+            sendToWhatsApp(this, contact, message)
+        }
+
     }
+
+    fun sendToWhatsApp(context: Context, phoneNumber: String, message: String) {
+        val messageSend = URLEncoder.encode(message, "UTF-8")
+        val url = "https://api.whatsapp.com/send?phone=$phoneNumber&text=$messageSend"
+        try {
+            val pm: PackageManager = context.packageManager
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES)
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        } catch (e: PackageManager.NameNotFoundException) {
+            Toast.makeText(
+                context,
+                "Whatsapp app not installed in your phone",
+                Toast.LENGTH_SHORT
+            ).show()
+            e.printStackTrace()
+        }
+    }
+
 }
